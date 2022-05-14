29fb0791804b4ee5e3a1f2bfb341dc5e0c37fd6f
sarahtell <sarah.lm.tell@gmail.com>
2022-05-14 14:04:14 +0200

    adds some animation

--- src/components/PokemonPresentation.tsx
+++ src/components/PokemonPresentation.tsx
@@ -1,4 +1,4 @@
-import { useCurrentFrame } from 'remotion';
+import { interpolate, useCurrentFrame, useVideoConfig } from 'remotion';
 import { PokemonStats } from '../App';
 import { PokemonSvg } from './PokemonSvg';
 
@@ -18,17 +18,39 @@ export type PokemonPresentationProps = {
 };
 
 export const PokemonPresentation = (props: PokemonPresentationProps) => {
+  const frame = useCurrentFrame();
+  const config = useVideoConfig();
+  const translate = interpolate(
+    frame,
+    [config.durationInFrames * 0.5, config.durationInFrames],
+    [0, 1]
+  );
+  const opacity = interpolate(
+    frame,
+    [config.durationInFrames * 0.9, config.durationInFrames],
+    [0, 1]
+  );
   return (
     <div
       className={`bg-gradient-to-b from-white to-blue-500 w-full flex ${
         props.statsToTheLeft ? 'flex-row-reverse' : 'flex-row'
       } items-center justify-center`}
     >
-      <div className="flex w-1/2">
+      <div
+        className="flex w-1/2"
+        style={{
+          transform: `translateX(${
+            props.statsToTheLeft
+              ? -config.width / 2 + (config.width / 2) * translate
+              : config.width / 2 - (config.width / 2) * translate
+          }px)`,
+        }}
+      >
         <PokemonSvg url={props.url} />
       </div>
       <div
         className={`pt-8 text-center ${props.statsToTheLeft ? 'pr-8' : 'pl-8'}`}
+        style={{opacity}}
       >
         <p className="capitalize font-bold">{props.name}</p>
         <p>
